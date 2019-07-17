42175d397ef4c621e5318d17eb6c8c5a2b1187b9
Gergő Papp <imgergopapp@gmail.com>
2019-07-17 09:30:44 +0200

    Add login-form component to angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
